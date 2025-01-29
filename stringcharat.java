e18bd92d573b668e08651bffaf7254ce749b3d77
vijay110311 <vijaysundar54321@gmail.com>
2025-01-29 11:59:17 +0530

    create new files

---
+++ stringcharat.java
@@ -0,0 +1,9 @@
+import java.lang.String;
+public class stringcharat {
+    public static void main(String[] args) {
+        String s="hello world";
+        char a=s.charAt(2);
+        System.out.println(a);
+    }
+
+}
